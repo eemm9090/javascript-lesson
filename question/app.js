69898c9b9d67da271b4ae109ce5d31ee568c9a1b
eemm9090 <51em605ek@gmail.com>
2024-10-28 16:23:22 +0900

    add：Q2 回答追記

--- question/app.js
+++ question/app.js
@@ -2,5 +2,12 @@
 let nickname = '倉ちゃん'
 let age = 27
 
-console.log('私ニックネームは'+nickname+'です。年齢は'+age+'です。')
+console.log('私ニックネームは'+nickname+'です。年齢は'+age+'です。');
+
+//Q２ 変数
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+
+let templateText =`私の好きな言語は${languages[0]}です。次は${languages[3]}nを勉強してみたいです。`
+
+console.log(templateText);
 
